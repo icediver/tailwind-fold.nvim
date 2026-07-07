3745dac5356994b179f1603e0ea397290f2d82dd
icediver <icedivet@live.ru>
2026-07-07 11:02:57 +0300

    feat: add cva() and other function support for tailwind-fold

--- queries/tsx/class.scm
+++ queries/tsx/class.scm
@@ -1,3 +1,7 @@
+;; ============================================
+;; ОРИГИНАЛЬНЫЕ ПАТТЕРНЫ (из плагина)
+;; ============================================
+
 (jsx_attribute
   (property_identifier) @_attribute_name
   (#any-of? @_attribute_name "class" "className" "style" "css" "tw")
@@ -9,7 +13,7 @@
   ])
 
 ;; ============================================
-;; ДОБАВЛЯЕМ ПОДДЕРЖКУ CVA И ДРУГИХ ФУНКЦИЙ
+;; ДОБАВЛЕННЫЕ ПАТТЕРНЫ ДЛЯ CVA И ДРУГИХ ФУНКЦИЙ
 ;; ============================================
 
 ;; Для cva() с обычной строкой
@@ -17,54 +21,32 @@
   function: (identifier) @_function_name
   (#eq? @_function_name "cva")
   arguments: (arguments
-    (string (string_fragment) @tailwind)
-  ))
+    (string (string_fragment) @tailwind)))
 
 ;; Для cva() с шаблонным литералом
 (call_expression
   function: (identifier) @_function_name
   (#eq? @_function_name "cva")
   arguments: (arguments
-    (template_string) @tailwind.inner
-  ))
+    (template_string) @tailwind.inner))
 
 ;; Для cn() (classnames)
 (call_expression
   function: (identifier) @_function_name
   (#eq? @_function_name "cn")
   arguments: (arguments
-    (string (string_fragment) @tailwind)
-  ))
+    (string (string_fragment) @tailwind)))
 
 ;; Для clsx()
 (call_expression
   function: (identifier) @_function_name
   (#eq? @_function_name "clsx")
   arguments: (arguments
-    (string (string_fragment) @tailwind)
-  ))
+    (string (string_fragment) @tailwind)))
 
 ;; Для twMerge()
 (call_expression
   function: (identifier) @_function_name
   (#eq? @_function_name "twMerge")
   arguments: (arguments
-    (string (string_fragment) @tailwind)
-  ))
-
-;; Для вариантов внутри объекта (cva({ variants: ... }))
-(call_expression
-  function: (identifier) @_function_name
-  (#eq? @_function_name "cva")
-  arguments: (arguments
-    (object
-      (pair
-        (property_identifier) @_key
-        (#any-of? @_key "variants" "defaultVariants")
-        (object
-          (pair
-            (property_identifier) @_variant
-            (object
-              (pair
-                (property_identifier) @_size
-                (string (string_fragment) @tailwind))))))))
+    (string (string_fragment) @tailwind)))
