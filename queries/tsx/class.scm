5129015439a490d5a6a941f82f7fcfe84b74f839
icediver <icedivet@live.ru>
2026-07-07 10:53:11 +0300

    feat: add cva() and other function support for tailwind-fold

--- queries/tsx/class.scm
+++ queries/tsx/class.scm
@@ -7,3 +7,64 @@
     (jsx_expression
       (template_string) @tailwind.inner)
   ])
+
+;; ============================================
+;; ДОБАВЛЯЕМ ПОДДЕРЖКУ CVA И ДРУГИХ ФУНКЦИЙ
+;; ============================================
+
+;; Для cva() с обычной строкой
+(call_expression
+  function: (identifier) @_function_name
+  (#eq? @_function_name "cva")
+  arguments: (arguments
+    (string (string_fragment) @tailwind)
+  ))
+
+;; Для cva() с шаблонным литералом
+(call_expression
+  function: (identifier) @_function_name
+  (#eq? @_function_name "cva")
+  arguments: (arguments
+    (template_string) @tailwind.inner
+  ))
+
+;; Для cn() (classnames)
+(call_expression
+  function: (identifier) @_function_name
+  (#eq? @_function_name "cn")
+  arguments: (arguments
+    (string (string_fragment) @tailwind)
+  ))
+
+;; Для clsx()
+(call_expression
+  function: (identifier) @_function_name
+  (#eq? @_function_name "clsx")
+  arguments: (arguments
+    (string (string_fragment) @tailwind)
+  ))
+
+;; Для twMerge()
+(call_expression
+  function: (identifier) @_function_name
+  (#eq? @_function_name "twMerge")
+  arguments: (arguments
+    (string (string_fragment) @tailwind)
+  ))
+
+;; Для вариантов внутри объекта (cva({ variants: ... }))
+(call_expression
+  function: (identifier) @_function_name
+  (#eq? @_function_name "cva")
+  arguments: (arguments
+    (object
+      (pair
+        (property_identifier) @_key
+        (#any-of? @_key "variants" "defaultVariants")
+        (object
+          (pair
+            (property_identifier) @_variant
+            (object
+              (pair
+                (property_identifier) @_size
+                (string (string_fragment) @tailwind))))))))
